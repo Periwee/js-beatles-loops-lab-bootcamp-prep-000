e8eeac7b4ff7cc7d016740b829726f3bed77046d
Esperanza Rodriguez <esperanza.rguezz@gmail.com>
2019-05-01 19:40:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+    var i=0;
+    var myArray = [];
+    var arrayLenght = musicians.lenght;
+    console.log(arrayLenght);
+    for(i=0;i<arrayLenght;i++){
+        myArray[i]=musicians[i]+ ' plays ' +instruments[i];
+    }
+    return myArray;
+}
